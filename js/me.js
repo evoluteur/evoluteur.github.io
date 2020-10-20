ddba1aa400b7c9903c824418f751c324f68cc73f
evoluteur <ogiulieri@gmail.com>
2020-10-20 01:24:34 -0700

    Braille tooltips + cleanups.

--- js/me.js
+++ js/me.js
@@ -539,9 +539,26 @@ function linkMore(id, preview){
 function setMosaic(id){
 	e(id+'2').innerHTML = mosaic(id);
 }
+const sectionTitles = {
+    code: 'Code',
+    comics: 'Graphic Novels',
+    recipes: 'Cooking',
+    movies: 'Movies',
+    fractals: 'Inspiration',
+    chakras: 'Meditation',
+    art: 'Namaste',
+}
 function setupPage(){
-	var pixAreas = ['code','comics','comics_us', 'comics_euro_us', 'recipes','movies','fractals','art','chakras']
-	pixAreas.forEach(setMosaic);
+    var pixAreas = ['code','comics','comics_us', 'comics_euro_us', 'recipes','movies','fractals','art','chakras']
+    const fn = isMobile ? 
+        setMosaic
+        : id => {
+            if(sectionTitles[id]){
+                document.getElementById(id+'-braille').innerHTML = br.braille(sectionTitles[id])
+            }
+            setMosaic(id)
+        }
+    pixAreas.forEach(fn);
 }
 
 let cTheme = ''
@@ -549,4 +566,130 @@ function toggleTheme(){
     const t = (cTheme==='-japan'?'':'-japan')
     document.getElementById('thememe').href='css/me'+t+'.css'
     cTheme = t
+}
+
+var br = {
+    braille: function (message) { 
+        var h=''; 
+        var myChar, prevCharNum, inQuote 
+
+        function BrChar(bPix, bAlt) {
+            return '<div class="br br-'+bPix+'"><span>'+bAlt+'</span></div>'
+            //return '<div class="br br-'+bPix+'" title="'+bAlt+'"></div>'
+        }
+
+        for (var i=0; i<message.length; i++) {
+            myChar = message.charAt(i);
+            if ((myChar>="a") && (myChar<="z")) { // a to z
+                    h+=BrChar(myChar, myChar);
+                    prevCharNum = false;            
+            } else if((myChar>="A") && (myChar<="Z")) { // A to Z
+                    h+=BrChar("cap", "Caps")+BrChar(myChar.toLowerCase(), myChar); // Caps
+                    prevCharNum = false;         
+            } else if((myChar>"0") && (myChar<="9")) {
+                    if (!prevCharNum){
+                            h+=BrChar("num", "Number");
+                    } 
+                    h+=BrChar(String.fromCharCode(myChar.charCodeAt(0) + 48), myChar); 
+                    prevCharNum = true;            
+            } else {
+                switch (myChar) {
+                    case " ": 
+                        h+=BrChar("sp", ""); //Space
+                        prevCharNum = false;
+                        break;
+                    case "0":
+                        if (!prevCharNum){
+                            h+=BrChar("num", "Number"); 
+                        }
+                        h+=BrChar("j", "0");      
+                        prevCharNum = true;
+                        break;
+                    case "\n":
+                        h+="<br><br>";
+                        nbCharsInLine = -1;
+                        prevCharNum = false;
+                        break;
+                    case ".":
+                        if (prevCharNum){
+                            h+=BrChar("dec", "."); 
+                        }
+                        else{
+                            h+=BrChar("period", ".");  
+                        }
+                        break;
+                    case "$":
+                        h+=BrChar("period", "$");
+                        prevCharNum = false;
+                        break;
+                    case "%":
+                        h+=BrChar("col", "%")+BrChar("p", "");
+                        prevCharNum = false;
+                        break;
+                    case "'":
+                        h+=BrChar("qs", "'"); 
+                        prevCharNum = false;
+                        break;
+                    case ",":
+                        h+=BrChar("comma", ",");
+                        prevCharNum = false;
+                        break;
+                    case "?":
+                        h+=BrChar("qu", "?"); 
+                        prevCharNum = false;
+                        break;
+                    case "(":
+                    case ")":
+                        h+=BrChar("par", "parenthesis"); 
+                        prevCharNum = false;
+                        break;
+                    case "*":
+                        h+=BrChar("ast", "*")+BrChar("ast", "*"); 
+                        prevCharNum = false;
+                        break;
+                    case "//":
+                        h+=BrChar("sla", "//"); 
+                        prevCharNum = false;
+                        break;
+                    case "!":
+                        h+=BrChar("ex", "!"); 
+                        prevCharNum = false;
+                        break;
+                    case "'": 
+                        if (inQuote)
+                            h+=BrChar("qc", "Close Quote"); 
+                        else
+                            h+=BrChar("qo", "Open Quote");  
+                        inQuote = !inQuote;
+                        prevCharNum = false;
+                        break;
+                    case ":":
+                        h+=BrChar("col", ":");
+                        prevCharNum = false;
+                        break;
+                    case ";":
+                        h+=BrChar("sc", ";"); 
+                        prevCharNum = false;
+                        break;
+                    case "[":
+                        h+=BrChar("cap", "[")+BrChar("par", "");
+                        break;
+                    case "]":
+                        h+=BrChar("par", "]")+BrChar("qs", ""); 
+                        break;
+                }
+            }
+        }
+        return h;
+    },
+
+    alphabet: function(){
+        var alpha='<div class="braille-doc2 alphabet">',
+            char;
+        for(var i=97;i<123;i++){
+            char=String.fromCharCode(i);
+            alpha+='<div><span>'+char+'</span><div class="br br-'+char+'"></div></div>'; 
+        }
+        return alpha+'</div>';
+    }
 }
